c9cf19b2f20e62c194cd5d805c9abd61488609f0
Derek Volker <d@dvolker.com>
2013-07-20 17:30:21 -0700

    level 5

--- player.rb
+++ player.rb
@@ -2,32 +2,45 @@ class Player
 
 
   def play_turn(warrior)
-    
-
-    [:forward,:backward,:left,:right].each do |dir|
-
-      if dir != warrior.direction_of_stairs and warrior.feel(dir).enemy? then
-        warrior.bind! dir
-        return 
-      end
-
+    if bind_and_attack(warrior) then
+      return
     end
-    # if warrior.feel(warrior.direction_of_stairs).captive? then
-    #   warrior.rescue! warrior.direction_of_stairs
-    # els
-    if warrior.feel(warrior.direction_of_stairs).enemy? then
-      warrior.attack! warrior.direction_of_stairs
-    elsif warrior.health < 20 then
-      warrior.rest!
+
+    if warrior.listen.count > 0 then
+      warrior.walk! warrior.direction_of(warrior.listen.first)
     else
-      if warrior.feel(:right).captive? then
-        warrior.rescue! :right
-        return
-      end
-      
       warrior.walk! warrior.direction_of_stairs
     end
+
   end
+
+  def bind_and_attack(warrior)
+    [:forward,:backward,:left,:right].each do |dir|
+      if warrior.feel(dir).enemy? then
+        if nearby_enemy_count(warrior) == 1 then
+          warrior.attack!(dir)
+          return true
+        else 
+          warrior.bind!(dir)
+          return true
+        end
+      end
+    end
+
+    if warrior.health < 20 then
+      warrior.rest!
+      return true
+    end
+
+    [:forward,:backward,:left,:right].each do |dir|
+      if warrior.feel(dir).captive? then
+        warrior.rescue! dir   
+        return true
+      end
+    end
+    return false
+  end
+
 
   def rescue_captive(warrior)
     [:forward,:backward,:left,:right].each do |dir|
